c9fc87dfc8f8535c132be45c51a473f4377ba995
sxend <arimitsu.k+github@gmail.com>
2014-04-29 13:46:45 +0900

    add SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -6,7 +6,7 @@ object Require4SBuild extends Build {
   lazy val root = Project(id = "Require4S", base = file("."), settings = Project.defaultSettings ++ Seq(
     organization := "arimitsu.sf",
     name := "Require4S",
-    version := "0.0.1",
+    version := "0.0.1-SNAPSHOT",
     scalaVersion := "2.11.0",
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % "2.1.4" % "test",
