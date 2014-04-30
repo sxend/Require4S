03552c97aad1ef9d6072e7e73aba59c0a51621cb
sxend <arimitsu.k+github@gmail.com>
2014-04-30 23:04:48 +0900

    modify build profile

--- project/build.scala
+++ project/build.scala
@@ -8,7 +8,7 @@ object Require4SBuild extends Build {
     name := "Require4S",
     version := "0.0.2-SNAPSHOT",
     crossScalaVersions := Seq("2.10.4", "2.11.0"),
-    publishTo := Some(Resolver.file("require4s", file("../maven-repo"))(Patterns(true, Resolver.mavenStyleBasePattern))),
+    publishTo := Some(Resolver.file("require4s", file("./maven-repo"))(Patterns(true, Resolver.mavenStyleBasePattern))),
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % "2.1.4" % "test",
       "org.scalaz" %% "scalaz-core" % "7.0.6"
